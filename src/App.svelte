809994bfaad6ce0e5c3458ce53ea91a0e41e5f86
Amelia Wattenberger <wattenberger@github.com>
2022-04-03 17:49:09 -0700

    Automatic update

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,7 @@
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <img width="100%" src="https://images.unsplash.com/photo-1587300003388-59208cc962cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80" />
 </main>
 
 <style>
