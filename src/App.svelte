356c8affb0b1bd51549e1cf8c49b9186397e45f6
Amelia Wattenberger <wattenberger@github.com>
2022-04-03 17:51:32 -0700

    Automatic update

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Welcome {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-    <img width="100%" src="https://images.unsplash.com/photo-1587300003388-59208cc962cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80" />
+    <img src="https://images.unsplash.com/photo-1587300003388-59208cc962cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80" />
 </main>
 
 <style>
@@ -22,6 +22,10 @@
 		font-size: 4em;
 		font-weight: 100;
 	}
+    img {
+      width: 60%;
+      margin-top: 3em;
+    }
 
 	@media (min-width: 640px) {
 		main {
